43e62464ef1f61b0f4d2289f1bc9f0179d94c53a
keves1 <keenan573@gmail.com>
2021-03-04 19:56:25 -0800

    Refactored, changed to capital-bikeshare since bay-wheels is not working

--- bikeshareapp/main.py
+++ bikeshareapp/main.py
@@ -1,58 +1,73 @@
-from bokeh.plotting import figure, curdoc
-from bokeh.layouts import column
-from bokeh.tile_providers import get_provider, Vendors
-from bokeh.models import GeoJSONDataSource
-from bokeh.models.tools import BoxZoomTool
 import json
 import math
 import pybikes
 
+from bokeh.io import curdoc
+from bokeh.layouts import column
+from bokeh.plotting import figure
+from bokeh.tile_providers import get_provider, Vendors
+from bokeh.models import GeoJSONDataSource
+
 
 def latlon_to_mercator(lat, lon):
     """
-    Converts coordinates from latitude/longitude to web mercator coordinates
-    Derived from the Java version shown here: http://wiki.openstreetmap.org/wiki/Mercator
-    :param lat:
-    :param lon:
-    :return:
-    """
+        Converts coordinates from latitude/longitude to web mercator coordinates
+        Derived from the Java version shown here: http://wiki.openstreetmap.org/wiki/Mercator
+        :param lat:
+        :param lon:
+        :return:
+        """
 
     radius = 6378137.0
     x = math.radians(lon) * radius
-    y = math.log(math.tan(math.radians(lat) / 2.0 + math.pi/4.0)) * radius
+    y = math.log(math.tan(math.radians(lat) / 2.0 + math.pi / 4.0)) * radius
 
     return y, x
 
 
-tile_provider = get_provider(Vendors.STAMEN_TERRAIN_RETINA)
+def gbsf_to_geojson(stations):
+    # convert data to geojson format
+    geo_dict = {}
+    geo_dict['type'] = 'FeatureCollection'
+    geo_dict['features'] = []
+    for station in stations:
+        station_dict = {}
+        station.latitude, station.longitude = latlon_to_mercator(station.latitude, station.longitude)
+        station_dict['geometry'] = {'type': 'Point', 'coordinates': [station.longitude, station.latitude]}
+        station_dict['type'] = 'Feature'
+        station_dict['id'] = station.extra['uid']
+        station_dict['properties'] = {'station name': station.name,
+                                      'bikes': station.bikes,
+                                      'free': station.free}
+        geo_dict['features'].append(station_dict)
+        geo_source = GeoJSONDataSource(geojson=json.dumps(geo_dict))
 
-# range bounds supplied in web mercator coordinates
-p = figure(x_range=(-13650000.00, -13600000.00), y_range=(4549761.18, 4540000.00),
-           x_axis_type="mercator", y_axis_type="mercator")
-BoxZoomTool.match_aspect = True
-p.add_tile(tile_provider)
+    return geo_source
 
-# get data in GBSF format
-bay_wheels = pybikes.get('bay-wheels')
-bay_wheels.update()
 
-# convert data to geojson format
-geo_dict = {}
-geo_dict['type'] = 'FeatureCollection'
-geo_dict['features'] = []
-for station in bay_wheels.stations:
-    station_dict = {}
-    station.latitude, station.longitude = latlon_to_mercator(station.latitude, station.longitude)
-    station_dict['geometry'] = {'type': 'Point', 'coordinates': [station.longitude, station.latitude]}
-    station_dict['type'] = 'Feature'
-    station_dict['id'] = station.extra['uid']
-    station_dict['properties'] = {'station name': station.name,
-                                  'bikes': station.bikes,
-                                  'free': station.free}
-    geo_dict['features'].append(station_dict)
+def get_data():
+    # get data in GBSF format
+    capital = pybikes.get('capital-bikeshare')
+    capital.update()
+    stations = capital.stations
+    geo_data = gbsf_to_geojson(stations)
 
-geo_source = GeoJSONDataSource(geojson=json.dumps(geo_dict))
+    return geo_data
 
-p.circle(x='x', y='y', size=5, color='green', alpha=0.8, source=geo_source)
 
-curdoc().add_root(column(p))
+def make_map():
+    tile_provider = get_provider(Vendors.STAMEN_TERRAIN_RETINA)
+
+    p = figure(x_range=(-8596413.91, -8558195.48), y_range=(4724114.13, 4696902.60),
+               x_axis_type="mercator", y_axis_type="mercator")
+    p.add_tile(tile_provider)
+
+    geo_data = get_data()
+
+    p.circle(x='x', y='y', size=5, color='green', alpha=0.8, source=geo_data)
+
+    return p
+
+
+fig = make_map()
+curdoc().add_root(column(fig))
